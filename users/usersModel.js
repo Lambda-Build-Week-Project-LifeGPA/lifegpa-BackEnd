e28261c2b67d20e47d4b411da5aef5c251f29a1d
sam-malanchuk <malanchuksam@gmail.com>
2019-09-19 21:30:59 -0400

    created a route to get all users

--- users/usersModel.js
+++ users/usersModel.js
@@ -21,5 +21,5 @@ async function insertNew(user) {
 }
 
 function selectAll() {
-  return null;
+  return db('users').select('id', 'name', 'email');
 }
--- users/usersRouter.js
+++ users/usersRouter.js
@@ -4,9 +4,15 @@ const jwt = require('jsonwebtoken');
 
 const Users = require('./usersModel.js');
 
-// test GET endpoint to see if route is configured correctly
+// GET provides a list of all the users
 router.get('/', (req, res) => {
-  res.json({ message: "Users endpoint is up!"});
+  Users.selectAll()
+    .then(users => {
+      res.status(201).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({message: "Server error getting all users"});
+    })
 });
 
 // POST requires an object {username, email, password}
